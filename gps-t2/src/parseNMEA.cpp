67bed79c64ed28f505b791982f6fc05ae5da096d
N0932809 <N0932809@my.ntu.ac.uk>
2020-10-15 15:46:55 +0100

    WellFormedNoFields = PASSED!

--- gps-t2/src/parseNMEA.cpp
+++ gps-t2/src/parseNMEA.cpp
@@ -1,14 +1,76 @@
 #include "earth.h"
 #include "parseNMEA.h"
+#include <iostream>
+#include <regex>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <regex>
+#include <locale>
+#include <string.h>
+
+using namespace std;
 
 namespace NMEA
 {
 
-  bool isWellFormedSentence(std::string)
+//REFACTOR WHOLE THING
+bool isWellFormedSentence(string NMEAString)
   {
-      // Stub definition, needs implementing
-      return false;
+	  bool isOk;
+	  string firstDelim = "$";
+	  string lastDelim = "*";
+	  int nmeaCodeLength = 5;
+	  string letterCheck = "[A-Z]+$";
+	  string check$ = NMEAString.substr (0,1); //Get first character
+
+
+	  // Refactor this part
+	  size_t s = NMEAString.find(firstDelim); // Find $ in string
+
+	  //Check if first char is $
+	  if(check$ == firstDelim){
+	    //Check if following char length is 5 chars
+	    string checkAZ = NMEAString.substr (1,5); //Get following chars
+	    if(checkAZ.length() == nmeaCodeLength){ //Currently only GPXXX however need to change to first string common
+	        //string checkAZ = NMEAString.substr (1,5); //Get following chars
+	  		bool onlyAlpha = regex_match(checkAZ, regex(letterCheck)); // Check if checkAZ contains only letters
+	  		//Check if those 5 chars are all letters
+    	    if(onlyAlpha == true){
+
+                string NMEACheck = "\\$([A-Z]{5})\\*[0-9A-Za-z]{2}"; // Checks NMEAString contains $ | 5 letters | any fields (if applicable) | * + checksum
+
+                bool hasStar = regex_match(NMEAString, regex(NMEACheck));
+
+    	        if(hasStar == true){
+
+    	            isOk = true;
+    	        }
+    	        else{
+    	            isOk = false;
+    	        }
+    	    }
+    	    else{
+    	        isOk = false;
+    	    }
+	  	}
+	  	else{
+	  	    isOk = false;
+	  	}
+	  }
+	  else{
+		  isOk = false;
+	  }
+
+      return isOk;
   }
+
+
+
+
+
 
   bool hasValidChecksum(std::string)
   {
